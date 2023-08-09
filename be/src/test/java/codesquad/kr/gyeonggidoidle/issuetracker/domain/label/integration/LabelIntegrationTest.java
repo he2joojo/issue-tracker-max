3f7e0ef0070826e42c0f7059e1faafd6c0702566
he2joo <hottestej6@gamil.com>
2023-08-09 18:09:37 +0900

    test: LabelIntegrationTest 작성 #63

--- be/src/test/java/codesquad/kr/gyeonggidoidle/issuetracker/domain/label/integration/LabelIntegrationTest.java
+++ be/src/test/java/codesquad/kr/gyeonggidoidle/issuetracker/domain/label/integration/LabelIntegrationTest.java
@@ -1,15 +1,19 @@
 package codesquad.kr.gyeonggidoidle.issuetracker.domain.label.integration;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 import codesquad.kr.gyeonggidoidle.issuetracker.annotation.IntegrationTest;
+import codesquad.kr.gyeonggidoidle.issuetracker.domain.label.controller.request.LabelCreateRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -19,7 +23,10 @@ public class LabelIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @DisplayName("라벨의 모드 정보를 가지고 온다.")
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DisplayName("라벨의 모든 정보를 가지고 온다.")
     @Test
     void testReadLabelPage() throws Exception {
         ResultActions resultActions = mockMvc.perform(get("/api/labels"));
@@ -31,5 +38,31 @@ public class LabelIntegrationTest {
                 .andExpect(jsonPath("$.labels.length()").value(4))
                 .andExpect(jsonPath("$.labels.[0].name").value("라벨 0"))
                 .andDo(print());
+    }
+
+    @DisplayName("라벨을 받아 저장한다.")
+    @Test
+    void create() throws Exception {
+        // given
+        LabelCreateRequest request = LabelCreateRequest.builder()
+                .name("label1")
+                .description("설명")
+                .backgroundColor("##")
+                .textColor("#")
+                .build();
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/api/labels")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(request)));
+
+        // then
+        resultActions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value(200))
+                .andDo(print());
+    }
+
+    private <T> String toJson(T data) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(data);
     }
 }
