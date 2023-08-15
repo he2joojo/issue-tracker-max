c11327d25786ba6b052f632d43109d8aaaf4f421
HeeJu Cho <121915790+he2joojo@users.noreply.github.com>
2023-08-16 02:30:55 +0900

    fix: github 계정으로 로그인 버그 수정 #93
    - OauthTokenResponse에 @JsonProperty을 추가해서 매핑이 안되는 문제 해결

--- be/src/main/java/codesquad/kr/gyeonggidoidle/issuetracker/domain/jwt/controller/response/OauthTokenResponse.java
+++ be/src/main/java/codesquad/kr/gyeonggidoidle/issuetracker/domain/jwt/controller/response/OauthTokenResponse.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class OauthTokenResponse {
+    @JsonProperty("access_token")
     private String accessToken;
     private String scope;
     @JsonProperty("token_type")
